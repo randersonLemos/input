2a0e88be98f056b53b4b65e747e55be7e1665e55
Randerson Lemos <randerson.lemos@gmail.com>
2019-11-23 12:30:11 -0300

    Changed file name from infos to resume. Created a class Well_name.

---
+++ scripts/resume.py
@@ -0,0 +1,24 @@
+from .well_name import Well_Name
+
+
+prod_lst = []
+prod_lst.append(Well_Name('PRK014'))
+prod_lst.append(Well_Name('PRK028'))
+prod_lst.append(Well_Name('PRK045'))
+prod_lst.append(Well_Name('PRK052'))
+prod_lst.append(Well_Name('PRK060'))
+prod_lst.append(Well_Name('PRK061'))
+prod_lst.append(Well_Name('PRK083'))
+prod_lst.append(Well_Name('PRK084'))
+prod_lst.append(Well_Name('PRK085'))
+prod_lst.append(Well_Name('Wildcat'))
+
+inje_lst = []
+inje_lst.append(Well_Name('IRK004',['IRK004_G','IRK004_W']))
+inje_lst.append(Well_Name('IRK028',['IRK028_G','IRK028_W']))
+inje_lst.append(Well_Name('IRK029',['IRK029_G','IRK029_W']))
+inje_lst.append(Well_Name('IRK036',['IRK036_G','IRK036_W']))
+inje_lst.append(Well_Name('IRK049',['IRK049_G','IRK049_W']))
+inje_lst.append(Well_Name('IRK050',['IRK050_G','IRK050_W']))
+inje_lst.append(Well_Name('IRK056',['IRK056_G','IRK056_W']))
+inje_lst.append(Well_Name('IRK063',['IRK063_G','IRK063_W']))
---
+++ scripts/well_name.py
@@ -0,0 +1,4 @@
+class Well_Name:
+    def __init__(self, name, nickname_lst=[]):
+        self.name = name
+        self.nickname_lst = nickname_lst
